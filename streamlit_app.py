255c10f633d3c71c98d55e934f219475ef6fca23
Mohammed Husamuddin <uni.husam@gmail.com>
2023-06-11 00:30:22 +0400

    Content Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,19 @@ def display_marketing_contributions():
     st.title("Marketing Contributions")
     
     marketing_contributions = [
-        "1. Created engaging email content and social media posts for events, resulting in a good increase in open rates and click-through rates compared to normal marketing campaign( This number is with respect to my experience in other clubs )"
-"2. Main Role: Designing Event Posts ( Instagram Visuals & Illustrations )"
+        "1. Created engaging email content and social media posts for events, resulting in a good increase in open rates and click-through rates compared to normal marketing campaign( This number is with respect to my experience in other clubs )",
+"2. Main Role: Designing Event Posts ( Instagram Visuals & Illustrations )",
 "3. Directed marketing activities and campaigns, ensuring alignment with organizational goals and brand guidelines. Achieved 95% consistency in brand messaging across all marketing channels."
-"4. Represented GDSC as the Marketing Team Representative at two physical inter-university GDSC Hackathons.(Only 2 were conducted this year)"
-"5. Developed visually appealing GDSC BPDC branding logos that received positive feedback from of GDSC members."
-"6. Established and maintained the GDSC LinkTree page, achieving a good user satisfaction rating for ease of navigation and access to relevant content."
-"7. Managed the GDSC Instagram page, increasing follower count by 200%(1st Year so Pretty Obvious, No Reference Point 😅) and achieving an average engagement rate of 63% through compelling visuals and interactive posts. ( These Numbers are calculated with the help of Instagram's Professional Tools Dashboard )"
-"8. Initiated discussions with Instagram to implement GDSC BPDC Verified stickers, aiming for a 15% increase in brand authenticity and trust next year.( In-Progress ) & ( Personal Initiative )"
-"9. Created and optimized GDSC email templates, which will result in a drastic increase in email engagement and conversion rates through A/B testing and data-driven improvements. ( Personal Initiative )"
- "   1. Currently testing it ( Please Find the Demo Email Templates in the Drive Folder )"
-"10. Led the design and production of GDSC merchandise, including bottles, caps, hoodies, and mugs, to promote brand visibility and foster a sense of community. ( Personal Initiative )"
-"11. Designed GDSC Council Hoodie designs that received positive feedback from 95% of council members ( In-Progress )"
-"12. Produced video teasers for Technofest GDSC BPDC Events, generating 200+ views and a 15% increase in event registrations."
+"4. Represented GDSC as the Marketing Team Representative at two physical inter-university GDSC Hackathons.(Only 2 were conducted this year)",
+"5. Developed visually appealing GDSC BPDC branding logos that received positive feedback from of GDSC members.",
+"6. Established and maintained the GDSC LinkTree page, achieving a good user satisfaction rating for ease of navigation and access to relevant content.",
+"7. Managed the GDSC Instagram page, increasing follower count by 200%(1st Year so Pretty Obvious, No Reference Point 😅) and achieving an average engagement rate of 63% through compelling visuals and interactive posts. ( These Numbers are calculated with the help of Instagram's Professional Tools Dashboard )",
+"8. Initiated discussions with Instagram to implement GDSC BPDC Verified stickers, aiming for a 15% increase in brand authenticity and trust next year.( In-Progress ) & ( Personal Initiative )",
+"9. Created and optimized GDSC email templates, which will result in a drastic increase in email engagement and conversion rates through A/B testing and data-driven improvements. ( Personal Initiative )",
+ "   1. Currently testing it ( Please Find the Demo Email Templates in the Drive Folder )",
+"10. Led the design and production of GDSC merchandise, including bottles, caps, hoodies, and mugs, to promote brand visibility and foster a sense of community. ( Personal Initiative )",
+"11. Designed GDSC Council Hoodie designs that received positive feedback from 95% of council members ( In-Progress )",
+"12. Produced video teasers for Technofest GDSC BPDC Events, generating 200+ views and a 15% increase in event registrations.",
 "13. Successfully marketed 13 out of 15 events( Was directly responsible for Marketing Them As a Marketing Manager Initially ), achieving an average event registration rate of 37% and receiving positive feedback on event promotion."
     ]
     
